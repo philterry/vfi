44f2d30931e76239451724f8fdadc809776f404c
root <root@localhost.localdomain>
2008-02-04 17:58:51 -0800

    rddma_dma_rio.h fix is for failed DMA address test... Snoop   bits in DMA descriptor were being set incorrectly.
      rddma_fabric.c has fabric call timeout hack... This fixes a
      bug in which bind_create when there is a large number of DMA descritors.
    
      rddma_local_ops.c fix is Phil's fix for problems generating
      DMA addresses when src and dest fragmentation don't match

--- rddma_fabric.c
+++ rddma_fabric.c
@@ -194,7 +194,9 @@ struct sk_buff *rddma_fabric_call(struct rddma_location *loc, int to, char *f, .
 		
 			init_waitqueue_head(&cb->wq);
 
-			if (wait_event_interruptible_timeout(cb->wq, (cb->rply_skb != NULL), to*HZ) == 0) {
+
+			/* Jimmy hack!  Increased timeout value 20x */
+			if (wait_event_interruptible_timeout(cb->wq, (cb->rply_skb != NULL), 20*to*HZ) == 0) {
 				kfree(cb);
 				return NULL;
 			}
